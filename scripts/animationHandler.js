ca048a86a9729cdbad6c296f987095acc3c8d39a
CoinCaribou2070 <73512308+CoinCaribou2070@users.noreply.github.com>
2020-12-30 14:37:08 -0700

    changes to scale melter

--- scripts/animationHandler.js
+++ scripts/animationHandler.js
@@ -1,5 +1,2 @@
 const advPlast = extendContent(GenericCrafter,"advanced-plastanium-compressor",{});
 advPlast.drawer = new DrawRotator();
-
-const scaleMelter = extendContent(GenericSmelter,"scale-melter",{});
-scaleMelter.drawer = new DrawRotator();
