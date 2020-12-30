da4dc3ec4fc9fadf8908eb4a25af2c69349a9fd2
CoinCaribou2070 <73512308+CoinCaribou2070@users.noreply.github.com>
2020-12-29 19:16:52 -0700

    hotfix

--- scripts/animationHandler.js
+++ scripts/animationHandler.js
@@ -1,2 +1,5 @@
 const advPlast = extendContent(GenericCrafter,"advanced-plastanium-compressor",{});
 advPlast.drawer = new DrawRotator();
+
+const scaleMelter = extendContent(GenericSmelter,"scale-melter",{});
+scaleMelter.drawer = new DrawRotator();
