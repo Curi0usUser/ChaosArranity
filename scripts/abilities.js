a5a9e3c0d9dd77a88fc91fce4678fa4a318b6f66
CoinCaribou2070 <73512308+CoinCaribou2070@users.noreply.github.com>
2021-01-10 21:40:12 -0700

    Added 2 new units
    Paralysis and Ravager

--- scripts/abilities.js
+++ scripts/abilities.js
@@ -13,36 +13,88 @@ defender.constructor = function(){
 };
 defender.abilities.add(new ForceFieldAbility(80, 5, 1000, 300), new RepairFieldAbility(25, 60, 80));
 
+
 //Adamantite Air
 const reliavent = extendContent(UnitType, "t1-reliavent", {});
 reliavent.constructor = function(){
   return extend(UnitEntity, {});
 };
 reliavent.abilities.add(new RepairFieldAbility(100, 300, 16));
+reliavent.immunities.add(incinerating);
+reliavent.immunities.add(StatusEffects.wet);
+reliavent.immunities.add(StatusEffects.melting);
+reliavent.immunities.add(StatusEffects.burning);
+reliavent.immunities.add(StatusEffects.muddy);
+reliavent.immunities.add(StatusEffects.sapped);
+reliavent.immunities.add(StatusEffects.sporeSlowed);
+reliavent.immunities.add(StatusEffects.tarred);
+reliavent.immunities.add(StatusEffects.shocked);
+reliavent.immunities.add(StatusEffects.corroded);
 
 const siloArray = extendContent(UnitType, "t2-silo-array", {});
 siloArray.constructor = function(){
   return extend(UnitEntity, {});
 };
 siloArray.abilities.add(new RepairFieldAbility(100, 240, 16));
+siloArray.abilities.add(new RepairFieldAbility(100, 300, 16));
+siloArray.immunities.add(incinerating);
+siloArray.immunities.add(StatusEffects.wet);
+siloArray.immunities.add(StatusEffects.melting);
+siloArray.immunities.add(StatusEffects.burning);
+siloArray.immunities.add(StatusEffects.muddy);
+siloArray.immunities.add(StatusEffects.sapped);
+siloArray.immunities.add(StatusEffects.sporeSlowed);
+siloArray.immunities.add(StatusEffects.tarred);
+siloArray.immunities.add(StatusEffects.shocked);
+siloArray.immunities.add(StatusEffects.corroded);
 
 const decimation = extendContent(UnitType, "t3-decimation", {});
 decimation.constructor = function(){
   return extend(UnitEntity, {});
 };
 decimation.abilities.add(new RepairFieldAbility(100, 180, 16));
+decimation.immunities.add(incinerating);
+decimation.immunities.add(StatusEffects.wet);
+decimation.immunities.add(StatusEffects.melting);
+decimation.immunities.add(StatusEffects.burning);
+decimation.immunities.add(StatusEffects.muddy);
+decimation.immunities.add(StatusEffects.sapped);
+decimation.immunities.add(StatusEffects.sporeSlowed);
+decimation.immunities.add(StatusEffects.tarred);
+decimation.immunities.add(StatusEffects.shocked);
+decimation.immunities.add(StatusEffects.corroded);
 
 const carnage = extendContent(UnitType, "t4-carnage", {});
 carnage.constructor = function(){
   return extend(UnitEntity, {});
 };
 carnage.abilities.add(new RepairFieldAbility(100, 120, 16));
+carnage.immunities.add(incinerating);
+carnage.immunities.add(StatusEffects.wet);
+carnage.immunities.add(StatusEffects.melting);
+carnage.immunities.add(StatusEffects.burning);
+carnage.immunities.add(StatusEffects.muddy);
+carnage.immunities.add(StatusEffects.sapped);
+carnage.immunities.add(StatusEffects.sporeSlowed);
+carnage.immunities.add(StatusEffects.tarred);
+carnage.immunities.add(StatusEffects.shocked);
+carnage.immunities.add(StatusEffects.corroded);
 
 const nightmare = extendContent(UnitType, "t5-nightmare", {});
 nightmare.constructor = function(){
   return extend(UnitEntity, {});
 };
 nightmare.abilities.add(new UnitSpawnAbility(UnitTypes.zenith, 1800, 0, 0), new UnitSpawnAbility(UnitTypes.zenith, 1800, 0, 0), new RepairFieldAbility(25, 60, 80));
+nightmare.immunities.add(incinerating);
+nightmare.immunities.add(StatusEffects.wet);
+nightmare.immunities.add(StatusEffects.melting);
+nightmare.immunities.add(StatusEffects.burning);
+nightmare.immunities.add(StatusEffects.muddy);
+nightmare.immunities.add(StatusEffects.sapped);
+nightmare.immunities.add(StatusEffects.sporeSlowed);
+nightmare.immunities.add(StatusEffects.tarred);
+nightmare.immunities.add(StatusEffects.shocked);
+nightmare.immunities.add(StatusEffects.corroded);
 
 //Adamantite Ground
 const melter = extendContent(UnitType, "t1-melter", {});
@@ -51,6 +103,15 @@ melter.constructor = function(){
 };
 melter.abilities.add(new RepairFieldAbility(100, 300, 16));
 melter.immunities.add(incinerating);
+melter.immunities.add(StatusEffects.wet);
+melter.immunities.add(StatusEffects.melting);
+melter.immunities.add(StatusEffects.burning);
+melter.immunities.add(StatusEffects.muddy);
+melter.immunities.add(StatusEffects.sapped);
+melter.immunities.add(StatusEffects.sporeSlowed);
+melter.immunities.add(StatusEffects.tarred);
+melter.immunities.add(StatusEffects.shocked);
+melter.immunities.add(StatusEffects.corroded);
 
 const evasculator = extendContent(UnitType, "t2-evasculator", {});
 evasculator.constructor = function(){
@@ -58,13 +119,22 @@ evasculator.constructor = function(){
 };
 evasculator.abilities.add(new RepairFieldAbility(100, 240, 16));
 evasculator.immunities.add(incinerating);
+evasculator.immunities.add(StatusEffects.wet);
+evasculator.immunities.add(StatusEffects.melting);
+evasculator.immunities.add(StatusEffects.burning);
+evasculator.immunities.add(StatusEffects.muddy);
+evasculator.immunities.add(StatusEffects.sapped);
+evasculator.immunities.add(StatusEffects.sporeSlowed);
+evasculator.immunities.add(StatusEffects.tarred);
+evasculator.immunities.add(StatusEffects.shocked);
+evasculator.immunities.add(StatusEffects.corroded);
 
 const blaster = extendContent(UnitType, "t3-blaster", {});
 blaster.constructor = function(){
   return extend(UnitEntity, {});
 };
 blaster.abilities.add(new RepairFieldAbility(100, 180, 16));
-blaster.immunities.add(incinerating);
+
 
 const catastrophere = extendContent(UnitType, "t4-catastrophere", {});
 catastrophere.constructor = function(){
@@ -72,6 +142,15 @@ catastrophere.constructor = function(){
 };
 catastrophere.abilities.add(new RepairFieldAbility(100, 120, 16));
 catastrophere.immunities.add(incinerating);
+catastrophere.immunities.add(StatusEffects.wet);
+catastrophere.immunities.add(StatusEffects.melting);
+catastrophere.immunities.add(StatusEffects.burning);
+catastrophere.immunities.add(StatusEffects.muddy);
+catastrophere.immunities.add(StatusEffects.sapped);
+catastrophere.immunities.add(StatusEffects.sporeSlowed);
+catastrophere.immunities.add(StatusEffects.tarred);
+catastrophere.immunities.add(StatusEffects.shocked);
+catastrophere.immunities.add(StatusEffects.corroded);
 
 const death = extendContent(UnitType, "t5-death", {});
 death.constructor = function(){
@@ -79,3 +158,45 @@ death.constructor = function(){
 };
 death.abilities.add(new RepairFieldAbility(100, 60, 16));
 death.immunities.add(incinerating);
+death.immunities.add(StatusEffects.wet);
+death.immunities.add(StatusEffects.melting);
+death.immunities.add(StatusEffects.burning);
+death.immunities.add(StatusEffects.muddy);
+death.immunities.add(StatusEffects.sapped);
+death.immunities.add(StatusEffects.sporeSlowed);
+death.immunities.add(StatusEffects.tarred);
+death.immunities.add(StatusEffects.shocked);
+death.immunities.add(StatusEffects.corroded);
+
+//Adamantite Naval
+const paralysis = extendContent(UnitType, "t1-paralysis", {});
+paralysis.constructor = function(){
+  return extend(UnitWaterMove, {});
+};
+paralysis.abilities.add(new RepairFieldAbility(100, 300, 16));
+paralysis.immunities.add(incinerating);
+paralysis.immunities.add(StatusEffects.wet);
+paralysis.immunities.add(StatusEffects.melting);
+paralysis.immunities.add(StatusEffects.burning);
+paralysis.immunities.add(StatusEffects.muddy);
+paralysis.immunities.add(StatusEffects.sapped);
+paralysis.immunities.add(StatusEffects.sporeSlowed);
+paralysis.immunities.add(StatusEffects.tarred);
+paralysis.immunities.add(StatusEffects.shocked);
+paralysis.immunities.add(StatusEffects.corroded);
+
+const ravager = extendContent(UnitType, "t2-ravager", {});
+ravager.constructor = function(){
+  return extend(UnitWaterMove, {});
+};
+ravager.abilities.add(new ForceFieldAbility(101.7, 10, 3000, 600), new RepairFieldAbility(100, 240, 16));
+ravager.immunities.add(incinerating);
+ravager.immunities.add(StatusEffects.wet);
+ravager.immunities.add(StatusEffects.melting);
+ravager.immunities.add(StatusEffects.burning);
+ravager.immunities.add(StatusEffects.muddy);
+ravager.immunities.add(StatusEffects.sapped);
+ravager.immunities.add(StatusEffects.sporeSlowed);
+ravager.immunities.add(StatusEffects.tarred);
+ravager.immunities.add(StatusEffects.shocked);
+ravager.immunities.add(StatusEffects.corroded);
